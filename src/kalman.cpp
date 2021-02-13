0a1218ed41cecdd2ae6e7b6dded40997756fbd21
EnacRobotique <robotique.enac@gmail.com>
2021-02-13 20:13:02 +0100

    début kalman

--- src/kalman.cpp
+++ src/kalman.cpp
@@ -3,6 +3,12 @@
 
 //Kalman: voir namespace
 
+/*
+    AFAIRE:
+    supprimer inutile ci_dessous
+    kalman_correct
+    fonction principale, appelée par navigator?
+*/
 
 /* ----------------------------------------------------------------------
 * Copyright (C) 2010-2012 ARM Limited. All rights reserved.
@@ -161,17 +167,53 @@ int32_t main(void)
   }
 }
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 namespace kalman{
+    /* X: etat
+    *  P: Cov état
+    *  F: systeme linearisé
+    *  B,U: mat commande et commande
+    *  Q: bruit sur commande
+    */
+
     //public:
     const uint16_t dim_etat=4, dim_cmde=1;
     float32_t *X=NULL;//état du systeme, alias X_data
     float32_t *P=NULL;//idem P_A, P_B
-    
+    float32_t *U=NULL;//commande
+
     //privé:
     arm_status statut;
+    //à créer, ou déjà en mémoire? -> supprimer lesz allocations statiques de X,P,U
     float32_t X_data[dim_etat];//c'est possible, ça ? [dim_etat]
     arm_matrix_instance_f32 *X_c;//calculs
 
+    float32_t U_data[dim_cmde];
+    arm_matrix_instance_f32 *U_c;
     float32_t P_data[dim_etat];
     arm_matrix_instance_f32 *P_c;
 
@@ -227,6 +269,7 @@ namespace kalman{
         arm_mat_init_f32(B, dim_etat, dim_cmde, (float32_t *)B_data);
         arm_mat_trans_f32(B, BT);
         arm_mat_init_f32(Q, dim_cmde, dim_cmde, (float32_t *)Q_data);
+        
 
         arm_mat_init_f32(calc1, dim_etat, 1, calc1_data);
         arm_mat_init_f32(calc2, dim_etat, 1, calc2_data);
@@ -235,15 +278,15 @@ namespace kalman{
         arm_mat_init_f32(calc5, dim_cmde, dim_etat, calc5_data);
 
         arm_mat_init_f32(X_c, dim_etat, 1, X_data);
+        arm_mat_init_f32(U_c, dim_cmde, 1, U_data);
         arm_mat_init_f32(P_c, dim_etat, dim_etat, P_data);
         
     }
 
     void kalman_predict(){
-        /* Xkm: etat,Pkm: Cov, F: systeme linearisé, B,U: commande, Q: bruit sur commande, idbuff: cf ci-dessous*/
         //Xkp1m = F*Xk +B*U
         statut=arm_mat_mult_f32(F, X_c, calc1);
-        statut=arm_mat_mult_f32(B, U, calc2);
+        statut=arm_mat_mult_f32(B, U_c, calc2);
         statut=arm_mat_add_f32(calc1, calc2, X_c);
 
         //Pkp1m = F*(Pk*FT) + B*(Q*BT)
@@ -254,4 +297,5 @@ namespace kalman{
         statut=arm_mat_add_f32(calc3, calc4, P_c);
         //débug: vérifier si statut = ARM_MATH_SUCCESS;
     }
+
 }
